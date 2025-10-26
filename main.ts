d027d67d7de90fed02bda820834fe73c8a08b73b
Cantorni <davidcantorbaez@gmail.com>
2025-10-26 18:57:27 -0500

    Taller 2

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { series } from './dataSerie.js';
 
 let seriesTbody: HTMLElement = document.getElementById('seriesTableBody')!;
 const totalSeasonElm: HTMLElement = document.getElementById("total-seasons")!;
+const cardContainer: HTMLElement = document.getElementById("serie-detail")!;
 
 renderSeriesInTable(series);
 totalSeasonElm.innerHTML = `${getAverageSeasons(series).toFixed(2)}`;
@@ -11,12 +12,18 @@ function renderSeriesInTable(seriesList: Serie[]): void {
   console.log('Desplegando series');
   seriesList.forEach((serie) => {
     let trElement = document.createElement("tr");
+
     trElement.innerHTML = `
       <th scope="row">${serie.id}</th>
-      <td><a href="${serie.url}">${serie.name}</a></td>
+      <td><a href="#" class="serie-link" data-id="${serie.id}">${serie.name}</a></td>
       <td>${serie.channel}</td>
       <td>${serie.season}</td>
     `;
+    trElement.querySelector(".serie-link")?.addEventListener("click", (event) => {
+      event.preventDefault();
+      showSerieDetail(serie);
+    });
+
     seriesTbody.appendChild(trElement);
   });
 }
@@ -27,3 +34,21 @@ function getAverageSeasons(seriesList: Serie[]): number {
   seriesList.forEach((s) => totalSeasons += s.season);
   return totalSeasons / seriesList.length;
 }
+
+function showSerieDetail(serie: Serie): void {
+  const cardContainer = document.getElementById("card-container")!;
+  cardContainer.innerHTML = `
+    <div class="card shadow-sm" style="width: 20rem;">
+      <img src="${serie.imageUrl}" class="card-img-top" alt="${serie.name}">
+      <div class="card-body">
+        <h5 class="card-title">${serie.name}</h5>
+        <p class="card-text">${serie.description}</p>
+        <a href="${serie.url}" target="_blank" class="text-info text-decoration-none">
+          ${serie.url}
+        </a>
+      </div>
+    </div>
+  `;
+}
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,24 @@
 import { series } from './dataSerie.js';
 let seriesTbody = document.getElementById('seriesTableBody');
 const totalSeasonElm = document.getElementById("total-seasons");
+const cardContainer = document.getElementById("serie-detail");
 renderSeriesInTable(series);
 totalSeasonElm.innerHTML = `${getAverageSeasons(series).toFixed(2)}`;
 function renderSeriesInTable(seriesList) {
     console.log('Desplegando series');
     seriesList.forEach((serie) => {
+        var _a;
         let trElement = document.createElement("tr");
         trElement.innerHTML = `
       <th scope="row">${serie.id}</th>
-      <td><a href="${serie.url}">${serie.name}</a></td>
+      <td><a href="#" class="serie-link" data-id="${serie.id}">${serie.name}</a></td>
       <td>${serie.channel}</td>
       <td>${serie.season}</td>
     `;
+        (_a = trElement.querySelector(".serie-link")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", (event) => {
+            event.preventDefault();
+            showSerieDetail(serie);
+        });
         seriesTbody.appendChild(trElement);
     });
 }
@@ -22,4 +28,19 @@ function getAverageSeasons(seriesList) {
     let totalSeasons = 0;
     seriesList.forEach((s) => totalSeasons += s.season);
     return totalSeasons / seriesList.length;
+}
+function showSerieDetail(serie) {
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = `
+    <div class="card shadow-sm" style="width: 20rem;">
+      <img src="${serie.imageUrl}" class="card-img-top" alt="${serie.name}">
+      <div class="card-body">
+        <h5 class="card-title">${serie.name}</h5>
+        <p class="card-text">${serie.description}</p>
+        <a href="${serie.url}" target="_blank" class="text-info text-decoration-none">
+          ${serie.url}
+        </a>
+      </div>
+    </div>
+  `;
 }
